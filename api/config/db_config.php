91e475fda0954f81a3d9cea5bcb70850fbd1b983
fmalabo-prog <fmalabo@csr-scc.edu.ph>
2026-02-24 19:53:04 +0800

    Update db_config.php

--- api/config/db_config.php
+++ api/config/db_config.php
@@ -1,7 +1,7 @@
 <?php
-
-ddefine('DB_HOST', 'localhost');
-define('DB_NAME', 'student_management');
+//Connection
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'csr');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 
