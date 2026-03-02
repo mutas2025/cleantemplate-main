78f681ee723422a305888345e9d58fdcb70ffc1a
mutas2025 <mutas@csr-scc.edu.ph>
2026-03-02 08:30:07 +0800

    Update login.php

--- api/login.php
+++ api/login.php
@@ -10,7 +10,7 @@
 // In a real, live application, you would typically log these errors to a file instead of showing them to the user.
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(E_ALL); ///
 
 // --- Main API Logic ---
 
